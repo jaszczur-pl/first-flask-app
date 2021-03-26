552f47556ea09540135feb47f10370fac307547b
jaszczur-pl <maciej.jaszczura17@gmail.com>
2021-03-26 20:17:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my first Flask application created for training purposes. Project is based on the tutorial "The Flask Mega-Tutorial" from this site https://blog.miguelgrinberg.com/
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['befitapp1@gmail.com']
+
+    POSTS_PER_PAGE = 3
+
+    LANGUAGES = ['en', 'pl']
+
+    MS_TRANSLATION_KEY = os.environ.get('MS_TRANSLATION_KEY')
